b6fc0af28d53d5e3ca6180286a699d0d844c7be9
davpark <djpark1996@gmail.com>
2021-02-18 23:17:54 -0500

    first fight iteration attempt 1

--- js/main.js
+++ js/main.js
@@ -12,19 +12,70 @@ $(document).ready(function() {
 
 const timer = ms => new Promise(res => setTimeout(res, ms))
 
+
+var incremental = 1;
+var attacked = false;
+var health = 5;
+// var userInput = document.getElementById("userInput").value;
+var rabbitHealth = 5;
+
 async function counter() {
 
-   var counter = 1;
-   while (counter < 7) {
+   while (incremental < 7) {
       // console.log(counter);
-      document.getElementById("counter").innerHTML = counter;
-      counter++;
+      document.getElementById("counter").innerHTML = incremental;
+      incremental++;
       await timer(1000);
-      if (counter === 6) {
-         counter = 1;
+      if (incremental === 6) {
+         incremental = 1;
       }
    }
 
   }
-  counter();
+counter();
 
+async function rabbitFight() {
+   for (rabbitHealth === 5; ; ) {
+      await timer(1000);
+      if (incremental === 6 && attacked === false) {
+         document.getElementById("rabbitFight").innerHTML = "Rabbit attacked for 4 dmg";
+         health = health - 4;
+         console.log("you've been hit, hp is: ", health);
+         if (health < 1) {
+            document.getElementById("rabbitFight").innerHTML = "ur ded lole";
+         }
+      }
+      if (incremental === 6 && attacked === true && rabbitHealth > 0) {
+         document.getElementById("rabbitFight").innerHTML = "Rabbit ran away";
+      }
+      if (rabbitHealth < 1) {
+         document.getElementById("rabbitFight").innerHTML = "You killed the rabbit";
+      }
+
+   }
+
+
+}
+rabbitFight()
+
+// input.addEventListener("keyup", function(event) {
+//    if (event.keyCode === 13) {
+//     event.preventDefault();
+//     document.getElementById("submitUserInput").click();
+//    }
+//  });
+
+function playerAction() {
+   var action = document.getElementById("performAction").value;
+   console.log(action);
+   if (action === 'heal' ) {
+      health = health + 3;
+      console.log("you healed, your hp is now: ", health);
+   }
+   if (action === 'attack') {
+      rabbitHealth = rabbitHealth - 3;
+      console.log(rabbitHealth);
+      attacked = true;
+   }
+}
+playerAction();
